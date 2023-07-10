8f7354503add6386777b2ec6a728eee1f94ec11b
Michele <michele.pioli@studenti.unicam.it>
2023-07-11 00:23:50 +0200

    mi sto pentendo delle mie decisioni nella vita

--- main.hs
+++ main.hs
@@ -1,9 +1,9 @@
+import System.IO (openFile, hGetContents, IOMode (ReadMode), hClose)
+import GHC.IO.IOMode
+
 
 -- posizione è rappresentata da valori int perché si tratta di una scacchiera
 type Position = (Int, Int)
-
--- type Virtu = Int
--- data Virtu = U | C | G | R
 
 -- Virtu può rappresentare la qualità del senpai o l'oggetto raccoglibile
 data Virtu a = U a
@@ -26,15 +26,30 @@ data Senpai = Senpai {
 
 -- i raccoglibili sono rappresentati solamente dalla posizione che occupano nella scacchiera
 data Checkboard = Checkboard {
+    size :: Int,
     senpai :: [Senpai],
     u :: [Virtu Position],
     c :: [Virtu Position],
     g :: [Virtu Position],
     r :: [Virtu Position]
 }
-
-constructDefaultSenpai (x, y) = constructSenpai (x, y, 0, 0, 0, 0)
-constructSenpai (x, y, u, c, g, r) = Senpai {
+    
+constructCheckboard fileContents = 
+    Checkboard {
+        size = n,
+        senpai = senpai,
+        u = Virtu pos,
+        c = Virtu pos,
+        g = Virtu pos,
+        r = Virtu pos,
+    }
+    where
+        n = read . drop 4 . head $ fileContents
+        senpai = 
+        pos =
+            
+constructDefaultSenpai x y = constructSenpai x y 0 0 0 0
+constructSenpai x y u c g r = Senpai {
     currentPosition = (x, y),
     stats = Stats {
         umilta = u,
@@ -44,10 +59,18 @@ constructSenpai (x, y, u, c, g, r) = Senpai {
     }
 }
 
+moveRight senpai = 
+    Senpai {
+        currentPosition = (x + 1, y),
+        stats = stats
+    }
+    where x = fst . posizione senpai
+        y = snd . posizione senpai
+        stats = stats senpai
+
 -- distanza è il numero di passi necessari per raggiungere l'altro punto
--- esempio chiamata: distance ((0,0), (1,1))
-distance :: (Position, Position) -> Int
-distance (a, b) = distx + disty
+distance :: Position -> Position -> Int
+distance a b = distx + disty
     where distx = fst b - fst a
           disty = snd b - snd a
 
@@ -60,6 +83,14 @@ position (x:y:xs) = (x, y)
 main = do
     putStrLn "Benvenuti al Dojo!"
     putStrLn "Inserire nome del file della configurazione:"
-    
-    -- aggiungi eccezioni
 
+    -- aggiungi eccezioni
+    fileName <- getLine
+    fileHandle <- openFile fileName ReadMode
+    fileContents <- hGetContents fileHandle
+    let checkBoard = constructCheckboard . lines $ fileContents
+    putStrLn "File caricato. Eseguendo ..."
+
+    print (runGame checkBoard)
+    putStrLn "Configurazione finale raggiunta!"
+    hClose fileHandle
